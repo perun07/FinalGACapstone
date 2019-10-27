a2c07ab61b7699137a88e4960354a7a1dd1d84b0
P Lowe <perun@reagan.com>
2019-10-27 00:55:50 -0400

    crud and posted

--- running_app/app/models/running_blog.rb
+++ running_app/app/models/running_blog.rb
@@ -1,2 +1,3 @@
 class RunningBlog < ApplicationRecord
+    mount_uploader :picture, PictureUploader
 end
--- running_app/config/routes.rb
+++ running_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/about'
   resources :running_blogs
+  root to: redirect('/running_blogs')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
